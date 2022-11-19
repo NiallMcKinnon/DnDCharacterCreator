ccfaed82099f8338a414ff709171478f1d5c7cd1
Niall McKinnon <nrm3009@uncw.edu>
2022-11-19 16:34:46 -0600

    Added callback functions for buttons

--- src/main/java/charactercreator/dndcharactercreator/HelloController.java
+++ src/main/java/charactercreator/dndcharactercreator/HelloController.java
@@ -2,17 +2,40 @@ package charactercreator.dndcharactercreator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public Button classButton;
+    public Button hairColorButton;
+    public Button nameButton;
+    public Button raceButton;
+    public Button eyeColorButton;
+    public Button pronounButton;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private final Player player = new Player();
+
+    public void classButton(ActionEvent actionEvent) {
+        System.out.println("Class Button Pressed!");
     }
 
-    public void onClassButtonClick(ActionEvent actionEvent) {
+    public void hairButton(ActionEvent actionEvent) {
+        System.out.println("Hair Button Pressed!");
+    }
+
+    public void nameButton(ActionEvent actionEvent) {
+        System.out.println("Name Button Pressed!");
+    }
+
+    public void raceButton(ActionEvent actionEvent) {
+        System.out.println("Race Button Pressed!");
+    }
+
+    public void eyeButton(ActionEvent actionEvent) {
+        System.out.println("Eye Button Pressed!");
+    }
+
+    public void pronounButton(ActionEvent actionEvent) {
+        System.out.println("Pronoun Button Pressed!");
     }
 }
